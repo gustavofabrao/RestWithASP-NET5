20ca8914e3272fd78e1d1d08e4f3c690c95a5068
Gustavo Abrao <gfbrao@gmail.com>
2022-06-28 14:59:54 -0300

    Content Negociation

---
+++ 08_RestWithASPNETUdemy_ContentNegociation/RestWithASPNETUdemy/RestWithASPNETUdemy/Program.cs
@@ -0,0 +1,75 @@
+using Microsoft.EntityFrameworkCore;
+using RestWithASPNETUdemy.Model.Context;
+using RestWithASPNETUdemy.Business;
+using RestWithASPNETUdemy.Business.Implementations;
+using RestWithASPNETUdemy.Repository; 
+using Serilog; 
+using RestWithASPNETUdemy.Repository.Generic;
+using Microsoft.Net.Http.Headers;
+
+var builder = WebApplication.CreateBuilder(args);
+
+Log.Logger = new LoggerConfiguration().WriteTo.Console().CreateLogger(); 
+
+// Add services to the container.
+builder.Services.AddControllers();
+
+
+// Connection 
+//var connection = builder.Configuration["MySQLConnection: MySQLConnectionString"];
+//var connection = builder.Configuration.GetConnectionString("MySQLConnection");
+var connection = "Server=localhost;DataBase=rest_with_asp_net_udemy;Uid=root;Pwd=admin123";
+builder.Services.AddDbContext<MySQLContext>(options => options.UseMySql(connection, ServerVersion.AutoDetect(connection)));
+
+if (builder.Environment.IsDevelopment())
+{
+    // cria as migrations
+    MigrateDatabase(connection);
+}
+
+builder.Services.AddMvc(options =>
+{
+    options.RespectBrowserAcceptHeader = true;
+    options.FormatterMappings.SetMediaTypeMappingForFormat("xml", MediaTypeHeaderValue.Parse("application/xml"));
+    options.FormatterMappings.SetMediaTypeMappingForFormat("json", MediaTypeHeaderValue.Parse("application/json"));
+}).AddXmlSerializerFormatters();
+
+// Versioning API
+builder.Services.AddApiVersioning();
+
+// Dependency Injection
+builder.Services.AddScoped<IPersonBusiness, PersonBusinessImplementation>(); 
+builder.Services.AddScoped<IBookBusiness, BookBusinessImplementation>();
+builder.Services.AddScoped(typeof(IRepository<>), typeof(GenericRepository<>));
+
+var app = builder.Build();
+
+// method migration
+void MigrateDatabase(string connection)
+{
+    try
+    {
+        var evolveConnection = new MySqlConnector.MySqlConnection(connection);
+        var evolve = new Evolve.Evolve(evolveConnection, msg => Log.Information(msg))
+        {
+            Locations = new List<string> { "db/migrations","db/dataset" },
+            IsEraseDisabled = true 
+        };
+        evolve.Migrate();
+    }
+    catch (Exception ex)
+    {
+        Log.Error("Database migration failed " + ex);
+        throw;
+    }
+}
+
+// Configure the HTTP request pipeline.
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
