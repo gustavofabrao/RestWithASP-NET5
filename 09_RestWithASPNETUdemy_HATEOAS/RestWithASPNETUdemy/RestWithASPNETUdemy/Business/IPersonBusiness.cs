159e11de4312a600f10d0bad80f0db75c95f433f
Gustavo Abrao <gfbrao@gmail.com>
2022-06-29 16:34:17 -0300

    HATEOAS

---
+++ 09_RestWithASPNETUdemy_HATEOAS/RestWithASPNETUdemy/RestWithASPNETUdemy/Business/IPersonBusiness.cs
@@ -0,0 +1,17 @@
+﻿using RestWithASPNETUdemy.Data.VO; 
+
+namespace RestWithASPNETUdemy.Business
+{
+    public interface IPersonBusiness
+    {
+        PersonVO Create(PersonVO PersonVO);
+
+        PersonVO Update(PersonVO PersonVO);
+
+        PersonVO FindByID(long id);
+
+        void Delete(long id);
+
+        List<PersonVO> FindAll(); 
+    }
+}
---
+++ 09_RestWithASPNETUdemy_HATEOAS/RestWithASPNETUdemy/RestWithASPNETUdemy/Hypermedia/ContentResponseEnricher.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Mvc.Routing;
+using RestWithASPNETUdemy.Hypermedia.Abstract;
+using System.Collections.Concurrent;
+
+namespace RestWithASPNETUdemy.Hypermedia
+{
+    public abstract class ContentResponseEnricher<T> : IResponseEnricher where T : ISupportsHyperMedia
+    {
+        public ContentResponseEnricher()
+        {
+
+        }
+
+        public bool CanEnrich(Type contentType)
+        {
+            return contentType == typeof(T) || contentType == typeof(List<T>);
+        }
+
+        protected abstract Task EnrichModel(T content, IUrlHelper urlHelper);
+
+        bool IResponseEnricher.CanEnrich(ResultExecutingContext response)
+        {
+            if(response.Result is OkObjectResult okObject)
+            {
+                return CanEnrich(okObject.Value.GetType());
+            }
+            return false;
+        }
+
+        public async Task Enrich(ResultExecutingContext response)
+        {
+            var urlHelper = new UrlHelperFactory().GetUrlHelper(response);
+            if (response.Result is OkObjectResult okObject)
+            { 
+                if(okObject.Value is T model)
+                {
+                    await EnrichModel(model, urlHelper);
+                }
+                else if (okObject.Value is List<T> collection)
+                {
+                    ConcurrentBag<T> bag = new ConcurrentBag<T>(collection);
+                    Parallel.ForEach(bag, (element) =>
+                    {
+                        EnrichModel(element, urlHelper);
+                    });
+                }
+
+            }
+            await Task.FromResult<object>(null);
+        }
+    }
+}
